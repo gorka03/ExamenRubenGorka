a06879253136e23d701a09a8542799c08faa2d2b
in2dm3-v <gorka.delaiglesiape@elorrieta-errekamari.com>
2024-11-14 17:24:14 +0100

    segundo commit

--- examen/app/src/main/java/com/example/examengorka/MainActivity.kt
+++ examen/app/src/main/java/com/example/examengorka/MainActivity.kt
@@ -3,15 +3,12 @@ package com.example.examengorka
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.examengorka.ui.theme.ExamenGorkaTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +24,93 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun Horizontal() {
+    Column {
+        Row {
+            Column {
+                Text("Hello Android!!")
+            }
 
+            Column {
+                Row {
+                    Text("Hello Android!!")
+                }
+                Row {
+                    Text("Hello Android!!")
+                }
+                Row {
+                    Text("Hello Android!!")
+                }
 
+            }
+        }
+
+        Row {
+            Text("Hello Android!!")
+
+        }
+
+        Column {
+            Row {
+                Button(onClick = {})
+
+                {
+                    Text("1")
+                }
+
+                Button(onClick = {})
+
+                {
+                    Text("4")
+                }
+                Button(onClick = {})
+
+                {
+                    Text("7")
+                }
+            }
+        }
+        Column {
+
+            Row {
+                Button(onClick = {})
+
+                {
+                    Text("2")
+                }
+
+                Button(onClick = {})
+
+                {
+                    Text("5")
+                }
+                Button(onClick = {})
+
+                {
+                    Text("8")
+                }
+            }
+        }
+
+        Column {
+            Row {
+                Button(onClick = {})
+
+                {
+                    Text("3")
+                }
+
+                Button(onClick = {})
+
+                {
+                    Text("6")
+                }
+                Button(onClick = {})
+
+                {
+                    Text("9")
+                }
+            }
+
+        }
+    }
 }
